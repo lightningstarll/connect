1229a8a53ea430d15698dee665dd54c5ad28f57a
lightningstarll <waghchoureatharva2004@gmail.com>
2024-09-24 21:26:56 +0530

    Delete Web dev directory

--- Web dev/protfolio.js
+++
@@ -1,7 +0,0 @@
-const logo = document.querySelector('.moving-logo');
-
-setInterval(() => {
-    const randomX = Math.floor(Math.random() * 20) - 10; // Move between -10 and 10px on X-axis
-    const randomY = Math.floor(Math.random() * 20) - 10; // Move between -10 and 10px on Y-axis
-    logo.style.transform = `translate(${randomX}px, ${randomY}px)`;
-}, 3000); // Move every 3 seconds
